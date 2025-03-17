e14e679aa427237dee13e42ef8ff4887fd2ba7c2
Lingling <pusanisa.jar@student.mahidol.edu>
2025-03-17 14:15:59 +0700

    Feature/manual checkin in classroom (#53)
    * checkin button in classroom page
    
    * linting fix
    
    * move to javascript in progress
    
    * checkin is now working from the add attendance page too
    
    * fix precommit
    
    * fix: js
    
    * fix: remove uneccessary import in application.ja
    
    * fix: only homeroom teacher be able to check-in their student

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,9 @@ Rails.application.routes.draw do
       post "checkin", to: "attendances#checkin"
     end
   end
+
   resources :attendances, only: [ :create ] do
-    get "/check_if_checked_in", to: "attendances#check_if_checked_in"
+    get "/status", to: "attendances#status"
   end
 
   # Profile
