62ee1506b26400e0f1a4d7072c64bf47065ddef9
chadakeng <chada.keng@gmail.com>
2025-03-17 19:16:59 +0700

    Fix: Syntax

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,8 @@
 class AttendancesController < ApplicationController
-  before_action :set_attendance, only: %i[ show edit update destroy ]
+  before_action :set_attendance, only: %i[show edit update destroy]
   before_action :authorize_admin_or_principal_or_system_or_teacher!
   include Pagy::Backend
+
   # GET /attendances or /attendances.json
   def index
     @attendances = Attendance.all
@@ -9,8 +10,7 @@ class AttendancesController < ApplicationController
 
   # GET /attendances/1 or /attendances/1.json
   def show
-    # This method is intentionally left empty because the show functionality
-    # is handled by the view and does not require any additional logic.
+    # This will render app/views/attendances/show.html.erb
   end
 
   # GET /attendances/new
@@ -18,16 +18,16 @@ class AttendancesController < ApplicationController
     @q = Student.active.ransack(params[:q])
     @students = @q.result(distinct: true)
     @attendances = Attendance.order(timestamp: :desc).limit(10)
+
     respond_to do |format|
-      format.html # For normal page loads
-      format.turbo_stream # For Turbo-powered live updates
+      format.html
+      format.turbo_stream
     end
   end
 
   # GET /attendances/1/edit
   def edit
-    # This method is intentionally left empty because the edit functionality
-    # is handled by the view and does not require any additional logic.
+    # This will render app/views/attendances/edit.html.erb
   end
 
   # POST /attendances or /attendances.json
@@ -37,6 +37,7 @@ class AttendancesController < ApplicationController
     existing_attendance = Attendance.where(student: student)
                                     .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
                                     .first
+
     if existing_attendance
       existing_attendance.destroy!
       render json: { status: "Destroy!" }, status: :ok
@@ -45,6 +46,7 @@ class AttendancesController < ApplicationController
       render json: { status: "success" }, status: :ok
     end
   end
+
   # PATCH/PUT /attendances/1 or /attendances/1.json
   def update
     respond_to do |format|
@@ -58,6 +60,7 @@ class AttendancesController < ApplicationController
     end
   end
 
+  # DELETE /attendances/1
   def destroy
     @attendance.destroy!
     respond_to do |format|
@@ -70,6 +73,7 @@ class AttendancesController < ApplicationController
     # This will render app/views/admin/scan_qr.html.erb
   end
 
+  # Check-in logic
   def checkin
     uid = params[:uid]
     hash = params[:hash]
@@ -83,7 +87,6 @@ class AttendancesController < ApplicationController
     end
 
     student = Student.find_by(uid: uid)
-
     if student.nil?
       redirect_to root_path, alert: "Student not found."
       return
@@ -93,7 +96,8 @@ class AttendancesController < ApplicationController
 
     if attendance.persisted?
       AttendanceMailer.check_in_notification(student, attendance).deliver_later
-      message = "Student checked in successfully at #{Time.current.strftime('%H:%M:%S')}. Parent has been notified."
+      message = "Student checked in successfully at #{Time.current.strftime('%I:%M:%S')}. Parent has been notified."
+
       respond_to do |format|
         format.json { render json: { success: true, message: message } }
         format.html { redirect_to admin_scan_qr_path, notice: message }
@@ -107,12 +111,27 @@ class AttendancesController < ApplicationController
     end
   end
 
+  # Check if a student has checked in today
   def check_if_checked_in
     Rails.logger.info("Checking if student is checked in")
+    student = Student.find_by(id: params[:attendance_id])
+
+    unless student
+      render json: { checked_in: false, message: "Student not found" }, status: :not_found
+      return
+    end
+
+    checked_in = Attendance.exists?(
+      student: student,
+      created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    )
+
+    render json: { checked_in: checked_in }
+  end
+
+  # Check if a student is checked in and authorized
   def status
     Rails.logger.info("Checking if student is checked in and authorized")
-    Rails.logger.info(params)
-
     student = Student.find_by(id: params[:attendance_id])
 
     unless student
@@ -120,34 +139,28 @@ class AttendancesController < ApplicationController
       return
     end
 
-    # Check if the student has already checked in today
     checked_in = Attendance.exists?(
       student: student,
       created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
     )
-    # get the classroom id of the student
+
     classroom_id = student.classroom_id
-    # get the teacher id of the classroom
     homeroom = Homeroom.find_by(classroom_id: classroom_id)
-    teacher_id = homeroom&.teacher_id # Safe navigation operator prevents nil errors
+    teacher_id = homeroom&.teacher_id
 
-    Rails.logger.info("Teacher ID: #{teacher_id}")
-    Rails.logger.info("Current User ID: #{current_user&.id}")
-
-    # Check if the user is authorized
     authorized = current_user.system? || (current_user.teacher? && current_user&.id == teacher_id)
+
     Rails.logger.info("Authorized: #{authorized} Checked In: #{checked_in}")
+
     render json: { checked_in: checked_in, authorized: authorized }
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_attendance
     @attendance = Attendance.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def attendance_params
     params.require(:attendance).permit(:student_id, :timestamp, :user_id)
   end
