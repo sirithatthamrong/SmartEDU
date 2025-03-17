fed0345d58521b91a152009a59d757700bd7f737
harrythe6arry <voravit.sri@student.mahidol.edu>
2025-03-17 14:27:28 +0700

    commented db file, not done yet + fix test

--- db/migrate/20250317032059_add_subscription_fields_to_payments.rb
+++ db/migrate/20250317032059_add_subscription_fields_to_payments.rb
@@ -1,7 +1,7 @@
 class AddSubscriptionFieldsToPayments < ActiveRecord::Migration[8.0]
   def change
-    add_column :payments, :stripe_subscription_id, :string
-    add_column :payments, :subscription_start, :datetime
-    add_column :payments, :subscription_end, :datetime
+    # add_column :payments, :stripe_subscription_id, :string
+    # add_column :payments, :subscription_start, :datetime
+    # add_column :payments, :subscription_end, :datetime
   end
 end
